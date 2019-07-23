4bc5ccd38f67b0ff701b26c01a2db1f7781d7570
enisberk <EnisBerk@users.noreply.github.com>
2019-07-23 14:32:02 -0400

    install venv

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
+apt update && apt install -y virtualenv
 virtualenv -p /usr/bin/python3 py3env
 source py3env/bin/activate
 python -V
 pip install scipy requests pillow lmdb opencv-python
-apt update && apt install -y libsm6 libxext6
+apt install -y libsm6 libxext6
 apt-get install -y libsm6 libxrender1 libfontconfig1
 python train.py
